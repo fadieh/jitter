3b7b719f5bdd42ead169ad957ab62bb29a3572cf
Fadie Hannona <fadiehannona@gmail.com>
2014-10-24 11:58:46 +0100

    RSpec files initialised and test/development databses added

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ require "https://rubygems.org"
 
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
+
+group :development, :test do
+	gem "rspec"
+end
--- server.rb
+++ server.rb
@@ -1,6 +1,8 @@
-env = ENV["RACK_ENV"] || "development"
+env = ENV["RACK_ENV"] || "development" # checking the environment we're in, default to development.
 
-DataMapper.setup(:default, "postgres://localhost/jitter_#{env}")
+DataMapper.setup(:default, "postgres://localhost/jitter_#{env}") # select database based on environment
+
+# then in spec helper, we specify the environment so the tests we're using are using the right database.
 
 require './lib/cheeps'
 
