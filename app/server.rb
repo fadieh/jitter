f879b8f225e8f31fe73b2266e826df6a7a380ea9
Fadie Hannona <fadiehannona@gmail.com>
2014-10-29 19:57:23 +0000

    preventing duplicate email registrations

--- app/server.rb
+++ app/server.rb
@@ -36,7 +36,7 @@ post '/users' do
 	session[:user_id] = @user.id
 	redirect to('/')
 	else
-	flash[:notice] = "Sorry, your passwords don't match"
-	erb:"/users/new"
+	flash.now[:errors] = @user.errors.full_messages
+	erb :"/users/new"
 	end
 end
