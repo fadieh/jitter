e0740df52403c7d2fc0e24fb20bb77b4819ae795
Fadie Hannona <fadiehannona@gmail.com>
2014-10-29 19:46:50 +0000

    preventing duplicate emails

--- lib/user.rb
+++ lib/user.rb
@@ -5,13 +5,14 @@ class User
 	include DataMapper::Resource
 
 	property :id, Serial
-	property :email, String
+	property :email, String, :unique => true
 	property :password_digest, Text
 
 	attr_reader :password
 	attr_accessor :password_confirmation
 
 	validates_confirmation_of :password
+	validates_uniqueness_of :email
 
 	def password=(password)
 		@password = password
