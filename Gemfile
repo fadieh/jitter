20298840203c22722e5e36b42325e06c3370b66e
Fadie Hannona <fadiehannona@gmail.com>
2014-10-29 19:28:13 +0000

    Passwords dont match, handling input errors

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'sinatra'
 gem 'bcrypt-ruby'
+gem 'rack-flash3'
 
 group :development, :test do
 	gem 'rspec'
--- app/server.rb
+++ app/server.rb
@@ -4,9 +4,12 @@ require './lib/cheeps'
 require './lib/user'
 require_relative 'data_mapper_setup'
 require_relative 'helpers/application'
+require 'rack-flash'
 
 enable :sessions
 set :session_secret, 'super secret'
+
+use Rack::Flash
 
 get '/' do
 	@cheeps = Cheep.all
@@ -20,6 +23,7 @@ post '/cheeps' do
 end
 
 get '/users/new' do
+	@user = User.new
 	erb :"users/new"
 end
 
@@ -28,6 +32,11 @@ post '/users' do
 				:password => params[:password],
 				:password_confirmation => params[:password_confirmation])
 	# need to establish that user so can access and list user id.
+	if @user.save
 	session[:user_id] = @user.id
 	redirect to('/')
+	else
+	flash[:notice] = "Sorry, your passwords don't match"
+	erb:"/users/new"
+	end
 end
