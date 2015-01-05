722dfe9c2882ee8f575218479e6ae7144a828d88
Fadie Hannona <fadiehannona@gmail.com>
2015-01-05 16:50:30 +0000

    added favourites to tweets

--- lib/cheeps.rb
+++ lib/cheeps.rb
@@ -6,10 +6,17 @@ class Cheep
 	property :message, Text
 	property :username, String
 
+	property :favourites, Integer, :default => 0 
+
 	attr_reader :password
 	attr_accessor :password_confirmation
 
 	validates_confirmation_of :password
+
+	def like
+		self.favourites += 1
+		self.save
+	end
 
 end
 
