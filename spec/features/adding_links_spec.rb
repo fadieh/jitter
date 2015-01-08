e2832dfc3b46c56a7d3c53112f26fa112910cfd6
Fadie Hannona <fadiehannona@gmail.com>
2015-01-08 23:12:46 +0000

    css

--- spec/features/adding_links_spec.rb
+++ spec/features/adding_links_spec.rb
@@ -1,9 +1,20 @@
 require 'spec_helper'
+require_relative 'helpers/session'
+
+include SessionHelpers
+
+before(:each) {
+    User.create(:email => "test@test.com",
+				:password => 'test',
+				:password_confirmation => 'test')
+				}
 
 feature "User adds a new cheep" do
 	scenario "when browsing the homepage" do
+		vist '/'
 		expect(Cheep.count).to eq(0)
-		visit '/'
+		sign_in('test@test.com', 'test')
+		visit '/main'
 		add_cheep("Hello world")
 		expect(Cheep.count).to eq(1)
 		cheep = Cheep.first
